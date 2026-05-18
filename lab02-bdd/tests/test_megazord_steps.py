51c233f82228a99474624cfbd6d451cb1d03535d
Victor Hugo Telles <tellesvictorhugo@gmail.com>
2026-05-18 23:32:31 +0000

    feat: Lab 02 - implementação do BDD

--- lab02-bdd/tests/test_megazord_steps.py
+++ lab02-bdd/tests/test_megazord_steps.py
@@ -2,26 +2,36 @@ from pytest_bdd import scenarios, given, when, then, parsers
 from src.calculadoras import CalculadoraDeEnergia
 
 # Informa ao pytest-bdd onde está o arquivo de texto (Gherkin)
-scenarios('../features/megazord.feature')
+scenarios("../features/megazord.feature")
+
 
 # GIVEN: O Setup (Estado inicial)
-@given(parsers.parse('que o cliente é tipo "{tipo_cliente}"'), target_fixture="contexto")
+@given(
+    parsers.parse('que o cliente é tipo "{tipo_cliente}"'), target_fixture="contexto"
+)
 def setup_cliente(tipo_cliente):
     # Passamos os dados via um dicionário que funciona como a "memória" do teste
-    raise NotImplementedError("Passo não implementado! Siga o roteiro (Passo 3).")
+    return {"tipo_cliente": tipo_cliente}
+
 
 @given(parsers.parse('a missão será na região "{regiao}"'))
 def setup_regiao(contexto, regiao):
-    raise NotImplementedError("Passo não implementado! Siga o roteiro (Passo 3).")
+    contexto["regiao"] = regiao
+
 
 # WHEN: A Ação do Sistema
-@when(parsers.parse('o Megazord calcular o custo de uma missão de valor {valor_base:d}'))
+@when(
+    parsers.parse("o Megazord calcular o custo de uma missão de valor {valor_base:d}")
+)
 def acao_calcular(contexto, valor_base):
-    # DICA: Instancie a CalculadoraDeEnergia aqui, chame o calcular_total
-    # e salve o resultado dentro do "contexto", para que o THEN consiga validar.
-    raise NotImplementedError("Passo não implementado! Siga o roteiro (Passo 3).")
+    calculadora = CalculadoraDeEnergia()
+    # Executa a nossa classe refatorada do Lab 01
+    contexto["custo_final"] = calculadora.calcular_total(
+        valor_base, contexto["tipo_cliente"], contexto["regiao"]
+    )
+
 
 # THEN: A Validação (Asserts)
-@then(parsers.parse('o custo final de energia deve ser {valor_esperado:d}'))
+@then(parsers.parse("o custo final de energia deve ser {valor_esperado:d}"))
 def checar_resultado(contexto, valor_esperado):
-    raise NotImplementedError("Passo não implementado! Siga o roteiro (Passo 3).")
+    assert contexto["custo_final"] == valor_esperado
