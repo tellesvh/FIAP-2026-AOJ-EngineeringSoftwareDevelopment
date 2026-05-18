c47ee01bf0fa4e9fdd3a530c4f9928806b116fbf
Victor Hugo Telles <tellesvictorhugo@gmail.com>
2026-05-18 23:50:21 +0000

    feat: Lab 03 - Implementação de lógica de imposto na função "calcular_total_mochila"

--- lab03-grasp/taverna.py
+++ lab03-grasp/taverna.py
@@ -16,10 +16,13 @@ class Inventario:
     def __init__(self):
         self.itens = []
 
-    def calcular_valor_total(self) -> float:
+    def calcular_valor_total(self, aplicar_imposto: bool) -> float:
         total = 0.0
         for item in self.itens:
-            total += item.preco
+            if item.nome.startswith("Poção") and aplicar_imposto:
+                total += item.preco * 1.10  # 10% de imposto
+            else:
+                total += item.preco
         return total
 
     def adicionar_novo_item(self, nome: str, preco: float):
@@ -42,9 +45,11 @@ class TaverneiroService:
         novo_item = ItemInventario("Espada Longa", 150.0)
         inventario.itens.append(novo_item)
 
-    def calcular_total_mochila(self, inventario: Inventario) -> float:
+    def calcular_total_mochila(
+        self, inventario: Inventario, aplicar_imposto: bool
+    ) -> float:
         # O Taverneiro não faz mais a conta na mão, ele só pergunta pro Inventário!
-        return inventario.calcular_valor_total()
+        return inventario.calcular_valor_total(aplicar_imposto)
 
 
 if __name__ == "__main__":
--- lab03-grasp/test_taverna.py
+++ lab03-grasp/test_taverna.py
@@ -1,27 +1,31 @@
 import pytest
 from taverna import TaverneiroService, Inventario, ItemInventario
 
+
 def test_venda_de_itens_simples():
     mochila = Inventario()
     servico = TaverneiroService()
-    
+
     servico.vender_pocao(mochila)
     servico.vender_espada(mochila)
-    
+
     # Valida se os itens entraram no inventario
     assert len(mochila.itens) == 2
-    
+
     # Valida o cálculo total base
-    total = servico.calcular_total_mochila(mochila)
+    total = servico.calcular_total_mochila(mochila, False)
     assert total == 200.0  # 50.0 da Poção + 150.0 da Espada
+
 
 def test_taxa_magica_desafio_final():
     mochila = Inventario()
     servico = TaverneiroService()
-    
-    servico.vender_pocao(mochila) # 50.0 + 10% = 55.0
-    servico.vender_espada(mochila) # 150.0 (sem taxa)
-    
+
+    servico.vender_pocao(mochila)  # 50.0 + 10% = 55.0
+    servico.vender_espada(mochila)  # 150.0 (sem taxa)
+
     # O total deve ser 205.0 peças de ouro, mas o código legado não tem a taxa.
-    total = servico.calcular_total_mochila(mochila)
-    assert total == 205.0, "O desafio da Taxa Mágica (10% na Poção) ainda não foi implementado!"
+    total = servico.calcular_total_mochila(mochila, True)
+    assert total == 205.0, (
+        "O desafio da Taxa Mágica (10% na Poção) ainda não foi implementado!"
+    )
